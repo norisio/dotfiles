a5db36ac5f9d3b849d6bdfba74f440e3ca24a256
norisio <k.exa.apostrophe@gmail.com>
2016-01-10 16:56:03 +0900

    20160110_3

--- deploy_vim.sh
+++ deploy_vim.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
-cd $HOME/dotfiles
-if [ ! -f $HOME/.vimrc ];then
-  ln -s ./.vimrc $HOME/.vimrc
+if ! which vim >/dev/null 2>&1;then
+  if which apt-get >dev/null 2>&1;then
+    sudo apt-get install vim
+  else
+    echo "vim found"
+  fi
 fi
-if [ ! -f $HOME/.gvimrc ];then
-  ln -s ./.gvimrc $HOME/.gvimrc
+
+cd $HOME
+if [ ! -d $HOME/dotfiles ];then
+  git clone https://github.com/norisio/dotfiles.git
+else
+  echo "repository dotfiles found"
 fi
+ln -si $HOME/dotfiles/.vimrc
+ln -si $HOME/dotfiles/.gvimrc
+
 if [ ! -d $HOME/neobundle.vim ]; then
   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
   sh ./install.sh
   rm ./install.sh
+else
+  echo "neobundle found"
 fi
 echo "created symbolic link to .(g)vimrc"
