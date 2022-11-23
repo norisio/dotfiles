8179804f5d540afaa4ba08dd926b30ae6ec106b0
Naoya Sakabe <k.exa.apostrophe@gmail.com>
2022-11-23 22:19:02 +0900

    c-p launches git_files instead of find_files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,7 +25,7 @@ require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     end
   }
 
